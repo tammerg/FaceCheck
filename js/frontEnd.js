aa7a50be01c95a25addcb0dfe9b7a1de6b38e0e5
evan bates <evan.bates@rutgers.edu>
2015-12-18 10:10:00 -0500

    organizing apis

--- js/frontEnd.js
+++ js/frontEnd.js
@@ -1,12 +1,6 @@
-window.urlBuild = {
-  apiAcc: {
-    faceApi: "https://apius.faceplusplus.com/v2/",
-    meth: "detection/detect?",
-    url: "http%3A%2F%2Fcdn.collider.com%2Fwp-content%2Fuploads%2Fanchorman-2-sequel-image-will-ferrell.jpg",
-    api_secret: "PTwMJGAq9Qnqg34EozZpH9mhwsy4mRbF",
-    api_key: "f53e2536752725f3bed49a4ede929058",
-    attributes: "age"
-  }
+window.ajxStore {
+  urlBuild: {
+    encodedURL: 0 
 }
 
 $(document).ready(function (){
@@ -14,23 +8,28 @@ $(document).ready(function (){
 
 function urlReader () {
   var imgLink = $("#url-value").val();
+  encodedURL = encodeURIComponent(imgLink);
+  }
 
-  var encodedURL = encodeURIComponent(imgLink);
+function facePlusAjax() {
+    apiAcc: {
+    faceApi: "https://apius.faceplusplus.com/v2/",
+    meth: "detection/detect?",
+    url: "http%3A%2F%2Fcdn.collider.com%2Fwp-content%2Fuploads%2Fanchorman-2-sequel-image-will-ferrell.jpg",
+    api_secret: "PTwMJGAq9Qnqg34EozZpH9mhwsy4mRbF",
+    api_key: "f53e2536752725f3bed49a4ede929058",
+    attributes: "age"
+    }
 
-  alert(encodedURL);
-
-  var fullUrl = urlBuild.apiAcc.faceApi + urlBuild.apiAcc.meth + "url=" + encodedURL + "&api_secret=" 
+var fullUrl = apiAcc.faceApi + apiAcc.meth + "url=" + encodedURL + "&api_secret=" 
       + urlBuild.apiAcc.api_secret + "&api_key=" + urlBuild.apiAcc.api_key + "&attribute=" + urlBuild.apiAcc.attributes;
-
-  alert(fullUrl);
-
 
 $.ajax ({
   type: "GET",
   url: fullUrl,
   success: faceCall
+
 });
-}
 
 function faceCall(imageData) {
   console.log(imageData);
@@ -39,9 +38,25 @@ function faceCall(imageData) {
   var newP = $("<p>");
   newP.append(callResults);
   $("#testApiInfo").append(newP);
-
- 
 }
+
+
+
+ apiAlch {
+    api_key: "c84b707045f2eaf44ac7896ca8638274c82489c4",
+
+ }
+
+
+
+$.ajax({
+  type: "GET",
+  url: http://gateway-a.watsonplatform.net/calls/url/URLGetRankedImageFaceTags
+
+
+});
+
+
 
 
 });
